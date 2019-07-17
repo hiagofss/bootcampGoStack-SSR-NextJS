df8e83f9ffba17d0e43b42e0b641b0e59019acb9
Hiago Souza <hiagofss98@gmail.com>
2019-07-17 11:35:47 -0300

    Estilização

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 import Link from 'next/link';
 import Head from 'next/head';
 
 import withAnalytics from '../src/hocs/withAnalytics';
+
+const Title = styled.h1`
+  color: #069;
+  font-size: 40px;
+`;
 
 const Home = () => (
   <div>
